30b12c71407b670b727ceaa3e1cfcca6d380b5f7
Mihai <mihaihustiuc100@gmail.com>
2023-10-31 13:52:23 +0000

    Put letter guess code inside a function

--- milestone_2.py
+++ milestone_2.py
@@ -3,8 +3,9 @@ word_list = ["grapes", "mango", "banana", "pineapple", "orange"]
 print(word_list)
 word = random.choice(word_list)
 print(word)
-guess = input("Please enter a single letter:",)
-if len(guess) == 1 and guess.isalpha() == True:
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
+def letter_guess():
+    guess = input("Please enter a single letter:",)
+    if len(guess) == 1 and guess.isalpha() == True:
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
